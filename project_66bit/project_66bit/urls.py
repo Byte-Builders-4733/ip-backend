3df4dc45e3fa9d5c34d99b5c018549999d1235ed
Михаил <mxoilslukin@gmail.com>
2023-06-13 13:44:08 +0500

    token

--- project_66bit/project_66bit/urls.py
+++ project_66bit/project_66bit/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from users.views import RegisterUserView
 from api.views import TestResultView
 
@@ -9,5 +9,6 @@ urlpatterns = [
     path('api-auth', include('rest_framework.urls')),
     path('auth/', include('djoser.urls')),
     path('register/', RegisterUserView.as_view(), name='registr'),
-    path('testsucc/', TestResultView.as_view())
+    path('testsucc/', TestResultView.as_view()),
+    re_path(r'ˆauth/', include('djoser.urls.authtoken')),
 ]
